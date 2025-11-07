7eab529b045f9b5e79ce7f57c2faebd05e46bc83
ahmed-rmdan <ahmedrmadan251998@gmail.com>
2025-11-07 23:42:24 +0200

    finish add post ui

--- app/dashboard/addpost/page.tsx
+++ app/dashboard/addpost/page.tsx
@@ -2,15 +2,15 @@
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-
-
-
+import { Textarea } from "@/components/ui/textarea"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Button } from "@/components/ui/button"
 export default function Login(){
   return(
-    <div className=" min-w-[75%]   flex items-center justify-center" >
-         <div className="bg-white flex flex-col w-[80%] items-center gap-[15px] p-5 rounded-2xl">
+    <div className=" min-w-[75%]   flex items-center justify-center p-5 " >
+         <div className="bg-white flex flex-col w-[98%] sm:w-[80%] items-center gap-[15px] p-5 rounded-2xl">
                <h1 className="text-main text-[9em] underline font-bold ">Add Post</h1>
-                <form className="flex flex-col w-[50%]  gap-[15px] ">
+                <form className="flex flex-col w-[85%] sm:w-[70%] xl:w-[50%]  gap-[15px] ">
                            <div className="grid gap-2 text-[2em]">
                                <Label className="text-[2.5em]" htmlFor="Main Title">Main Title</Label>
                                   <Input
@@ -20,12 +20,12 @@ export default function Login(){
                                  required
                                      />
                                      {/* <p className="text-main font-semibold text-[1.8em] ">* dsadsadsa</p> */}
-                            </div>
-                            <div className="grid gap-2 text-[2em]">
-                               <Label className="text-[2.5em]" htmlFor="Description">Description</Label>
+                            </div>                  
+                           <div className="grid gap-2 text-[2em]">
+                               <Label className="text-[2.5em]" htmlFor="Main Title">Main Image</Label>
                                   <Input
-                                 id="Description"
-                                  type="text"
+                                 id="Main Title"
+                                  type="file"
                                 placeholder=""
                                  required
                                      />
@@ -40,7 +40,22 @@ export default function Login(){
                                  required
                                      />
                                      {/* <p className="text-main font-semibold text-[1.8em] ">* dsadsadsa</p> */}
-                            </div>                                                          
+                            </div>
+                            <div className="grid gap-2 text-[2em]">
+                               <Label className="text-[2.5em]" htmlFor="content">Content</Label>
+                                  <Textarea
+                                 id="content"                                 
+                                placeholder=""
+                                 required
+                                     />
+                                     {/* <p className="text-main font-semibold text-[1.8em] ">* dsadsadsa</p> */}
+                            </div>
+                              <div className="flex items-center gap-3 mt-2">
+                                      <Checkbox id="terms" />
+                                    <Label className="font-semibold text-[3.5em]" htmlFor="terms">Add the post to the main Home page Slider</Label>
+                             </div>  
+                                 {/* <p className="text-main font-semibold text-[2em] ">* dsadsadsa</p> */}
+                               <Button className="w-[30%] self-center !text-[4em] mt-[10px] hover:cursor-pointer bg-main hover:bg-gray-500"> Submit </Button>                                                     
                  </form>
          </div>
 
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
     <HeaderDashboard></HeaderDashboard>
-      <main className=" flex flex-row items-center justify-start  w-full bg-sec text-[2.5px] sm:text-[4px]   lg:text-[4px]  xl:text-[4px] 2xl:text-[4.5px] ">
+      <main className=" flex flex-row items-center justify-start  w-full bg-sec text-[3px] sm:text-[4px]   lg:text-[4px]  xl:text-[4px] 2xl:text-[4.5px]  ">
         <Dashboard></Dashboard>
         {children}
         </main>
